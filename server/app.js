b9bf00a75e926578aaabdf97b47db6600918f739
neo joram <neojoram12@gmail.com>
2024-03-24 21:47:59 +0200

    bdy

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const pool = require("./connection");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 const createTablesQuery = `
   CREATE TABLE IF NOT EXISTS users (
